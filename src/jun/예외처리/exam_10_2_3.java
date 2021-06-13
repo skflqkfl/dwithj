53b16282277fd8ad33eedb7e380a323c0688609b
parkj <parkj@pjy-t14>
2021-06-13 20:19:03 +0900

    박준영 커밋테스트

---
+++ src/jun/예외처리/exam_10_2_3.java
@@ -0,0 +1,10 @@
+package jun.예외처리;
+
+public class exam_10_2_3 {
+	public static void main(String[] args) {
+		String str1 = new String("박준영");
+		System.out.println("str1 : " + Integer.parseInt(str1));
+		
+		
+	}
+}
