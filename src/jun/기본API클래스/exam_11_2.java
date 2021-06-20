def76775ea9e294046978bf353f4057bc3526318
parkj <parkj@pjy-t14>
2021-06-20 22:41:36 +0900

    커밋

--- src/jun/기본API클래스/exam_11_2.java
+++ src/jun/기본API클래스/exam_11_2.java
@@ -1,7 +1,10 @@
 package jun.기본API클래스;
 
-public class exam_11_2 {
+public class exam_11_2 implements Cloneable {
 	public static void main(String[] args) {
 		System.out.println("");
+		Object object = new Object();
+		String string = new String();
+		
 	}
 }
